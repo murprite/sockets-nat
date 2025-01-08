8092ec68301f1e51893b754c45e23de045686e90
murprite <murprite@gmail.com>
2025-01-08 16:51:00 +0700

    initial commit

---
+++ frontend/vite.config.ts
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  server: {
+    port: 5000,
+},
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    outDir: "../frontend-dist",
+  },
+  root: "./frontend"
+})
